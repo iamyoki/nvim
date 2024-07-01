abdfb15e4106cf79172f738c6055c7ea7196003a
Yoki <yokiyuqian@gmail.com>
2024-07-02 07:32:47 +0800

    disable sys clipboard add few sys cliboard keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map({ "n", "v" }, "<C-y>", '"+y', { desc = "Yank into sys clipboard" })
+map({ "n", "v" }, "<C-d>", '"+d', { desc = "Delete into sys clipboard" })
+map({ "n", "v" }, "<C-x>", '"+cc', { desc = "Cut into sys clipboard" })
--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -2,11 +2,30 @@ return {
   -- add catppuccin
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
+  -- add kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+              bg_dim = "none",
+            },
+          },
+        },
+      },
+    },
+  },
+
   -- load color schema
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      -- colorscheme = "catppuccin-frappe",
+      colorscheme = "kanagawa",
     },
   },
 }
