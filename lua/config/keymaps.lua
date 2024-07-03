732b4b5fec7764d3c6010446b5482dbcead52d6b
Yoki <yokiyuqian@gmail.com>
2024-07-03 10:12:16 +0800

    add toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,10 @@
 
 local map = vim.keymap.set
 
+-- clipboard
 map({ "n", "v" }, "<C-y>", '"+y', { desc = "Yank into sys clipboard" })
 map({ "n", "v" }, "<C-d>", '"+d', { desc = "Delete into sys clipboard" })
 map({ "n", "v" }, "<C-x>", '"+cc', { desc = "Cut into sys clipboard" })
+
+-- toggleterm
+map({ "n", "v", "t" }, "<leader>t", "<cmd>exe v:count1 . 'ToggleTerm'<CR>", { desc = "Toggle terminal" })
