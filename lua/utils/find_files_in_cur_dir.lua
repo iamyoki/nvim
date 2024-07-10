621c45221ae8a16cbcb85ffb5a7c42d2e28aa25c
Yoki <74389358+iamyoki@users.noreply.github.com>
2024-07-10 16:29:41 +0800

    up find_files_in_cur_dir

--- lua/utils/find_files_in_cur_dir.lua
+++ lua/utils/find_files_in_cur_dir.lua
@@ -1,9 +1,25 @@
 local builtin = require("telescope.builtin")
 local utils = require("telescope.utils")
+local action_state = require("telescope.actions.state")
+
+local function find_files_in_dir(dir)
+  local function attach_mappings(_, map)
+    map("n", "../", function(_prompt_bufnr)
+      local current_picker = action_state.get_current_picker(_prompt_bufnr)
+      local current_cwd = current_picker.cwd
+      local parent_dir = vim.fn.fnamemodify(current_cwd, ":h")
+
+      find_files_in_dir(parent_dir)
+    end)
+    return true
+  end
+
+  builtin.find_files({ cwd = dir, prompt_title = "Files in " .. dir, attach_mappings = attach_mappings })
+end
 
 local function find_files_in_cur_dir()
   local buffer_dir = utils.buffer_dir()
-  builtin.find_files({ cwd = buffer_dir, prompt_title = "Files in " .. buffer_dir })
+  find_files_in_dir(buffer_dir)
 end
 
 return find_files_in_cur_dir
